7d66f5c97386348166b867bc398c06c36fcc3416
Gabriel Tavernini <taverninigabriel@gmail.com>
2026-02-04 16:03:56 +0100

    add bending to editor

--- editor.py
+++ editor.py
@@ -59,6 +59,13 @@ def main():
         gui_controls['k1'] = server.gui.add_slider("K 1", -1, 1.0, 0.01, 0.0)
         gui_controls['k2'] = server.gui.add_slider("K 2", -1, 1.0, 0.01, 0.0)
 
+        gui_controls['k_z'] = server.gui.add_slider("Bend Z (k)", 0.0, 10.0, 0.01, 0.0)
+        gui_controls['a_z'] = server.gui.add_slider("Bend Z (deg)", 0.0, 360.0, 5.0, 0.0)
+        gui_controls['k_x'] = server.gui.add_slider("Bend X (k)", 0.0, 10.0, 0.01, 0.0)
+        gui_controls['a_x'] = server.gui.add_slider("Bend X (deg)", 0.0, 360.0, 5.0, 0.0)
+        gui_controls['k_y'] = server.gui.add_slider("Bend Y (k)", 0.0, 10.0, 0.01, 0.0)
+        gui_controls['a_y'] = server.gui.add_slider("Bend Y (deg)", 0.0, 360.0, 5.0, 0.0)
+
     # 4. Functions
     def render_all():
         """Regenerates and displays the mesh."""
@@ -88,6 +95,7 @@ def main():
         e = pred_handler.exponents[B_IDX, p_idx]
         r = R.from_matrix(pred_handler.rotation[B_IDX, p_idx]).as_euler('xyz', degrees=True)
         k = pred_handler.tapering[B_IDX, p_idx]
+        b = pred_handler.bending[B_IDX, p_idx]
 
         gui_controls['tx'].value = float(t[0])
         gui_controls['ty'].value = float(t[1])
@@ -102,6 +110,12 @@ def main():
         gui_controls['e2'].value = float(e[1])
         gui_controls['k1'].value = float(k[0])
         gui_controls['k2'].value = float(k[1])
+        gui_controls['k_z'].value = float(b[0])
+        gui_controls['a_z'].value = float(np.degrees(b[1]))
+        gui_controls['k_x'].value = float(b[2])
+        gui_controls['a_x'].value = float(np.degrees(b[3]))
+        gui_controls['k_y'].value = float(b[4])
+        gui_controls['a_y'].value = float(np.degrees(b[5]))
         
         is_loading = False # Re-enable sync
         render_all()
@@ -122,6 +136,11 @@ def main():
         pred_handler.rotation[B_IDX, p_idx] = rot_obj.as_matrix()
 
         pred_handler.tapering[B_IDX, p_idx] = [gui_controls['k1'].value, gui_controls['k2'].value]
+        pred_handler.bending[B_IDX, p_idx] = [
+            gui_controls['k_z'].value, np.radians(gui_controls['a_z'].value),
+            gui_controls['k_x'].value, np.radians(gui_controls['a_x'].value),
+            gui_controls['k_y'].value, np.radians(gui_controls['a_y'].value)
+        ]
 
         render_all()
 
